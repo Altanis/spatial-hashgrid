36e8be2cada36a00097322e62e9bcf332e4dab18
Altanis <38045884+Altanis@users.noreply.github.com>
2023-02-24 20:27:21 -0500

    e

--- shg.rs
+++ shg.rs
@@ -97,7 +97,7 @@ fn main() {
 	/* Insert entities. */
     grid.insert(0, 0, 10, 10, 0);
     grid.insert(9, 9, 10, 10, 1);
-    grid.insert(1, 1, 100, 100, 1);
+    grid.insert(1, 1, 100, 100, 2);
 
 	/* Query entities. */
     let mut result = grid.query(0, 0, 10, 10);
